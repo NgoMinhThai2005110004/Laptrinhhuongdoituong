b7c83369d818355706d9ff38a995a217cc2d467a
NgoHuyPhan <phanhuyngonvh2707@gmail.com>
2022-01-18 23:24:46 +0700

    Lan 1

--- src/oo/EmployeeTestDrive.java
+++ src/oo/EmployeeTestDrive.java
@@ -2,7 +2,7 @@ package oo;
 
 public class EmployeeTestDrive{
     public static void main(String[] args) {
-        Employee e1mployee = new Employee();
+        Employee employee = new Employee();
 
         employee.show();
     }
